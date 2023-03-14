3db47ae176fc0e57f64fce421a73bf1934d855a9
Kevin Kevin Thomas <kevin@kevincthomas.com>
2023-03-14 12:51:47 -0400

    Update base.py
    The conversational agent at /langchain/agents/conversational/base.py looks to have a regex that isn't good for pulling out multiline Action Inputs, whereas the
    mrkl agent at /langchain/agents/mrkl/base.py has a good regex for pulling out multiline Action Inputs
    When I switched the regex FROM the top one at /langchain/agents/conversational/base.py TO the bottom one at /langchain/agents/mrkl/base.py, it worked for me!
    
    def _extract_tool_and_input(self, llm_output: str) -> Optional[Tuple[str, str]]:
    if f"{self.ai_prefix}:" in llm_output:
    return self.ai_prefix, llm_output.split(f"{self.ai_prefix}:")[-1].strip()
    regex = r"Action: (.*?)[\n]Action Input: (.)"
    match = re.search(regex, llm_output)
    
    def get_action_and_input(llm_output: str) -> Tuple[str, str]:
    """Parse out the action and input from the LLM output.
    
    Note: if you're specifying a custom prompt for the ZeroShotAgent,
    you will need to ensure that it meets the following Regex requirements.
    The string starting with "Action:" and the following string starting
    with "Action Input:" should be separated by a newline.
    """
    if FINAL_ANSWER_ACTION in llm_output:
        return "Final Answer", llm_output.split(FINAL_ANSWER_ACTION)[-1].strip()
    **regex = r"Action: (.*?)[\n]*Action Input: (.*)"
    match = re.search(regex, llm_output, re.DOTALL)**

--- langchain/agents/conversational/base.py
+++ langchain/agents/conversational/base.py
@@ -79,7 +79,7 @@ class ConversationalAgent(Agent):
         if f"{self.ai_prefix}:" in llm_output:
             return self.ai_prefix, llm_output.split(f"{self.ai_prefix}:")[-1].strip()
         regex = r"Action: (.*?)[\n]*Action Input: (.*)"
-        match = re.search(regex, llm_output)
+        match = re.search(regex, llm_output, re.DOTALL)
         if not match:
             raise ValueError(f"Could not parse LLM output: `{llm_output}`")
         action = match.group(1)
